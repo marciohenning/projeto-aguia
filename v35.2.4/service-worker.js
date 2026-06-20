2e601cdbf1585896a17ab315d87aa72bc1122278
marcio henning <marciohenning@gmail.com>
2026-06-20 14:04:45 -0300

    Delete v35.2.4 directory

--- v35.2.4/service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='aguia-v35-2-4';
-const FILES=["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./data/perguntas/catecismo.json", "./data/perguntas/ciencias.json", "./data/perguntas/educacao_financeira.json", "./data/perguntas/geografia.json", "./data/perguntas/geometria.json", "./data/perguntas/historia.json", "./data/perguntas/index.json", "./data/perguntas/ingles.json", "./data/perguntas/latim.json", "./data/perguntas/matematica.json", "./data/perguntas/portugues.json"];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))) });
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
